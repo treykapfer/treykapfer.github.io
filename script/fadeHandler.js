485802be79bb53f43e96745e875df1bd603fac1a
Trey K <80800860+treykapfer@users.noreply.github.com>
2021-07-08 15:16:28 -0500

    add js scripts and border css

--- script/fadeHandler.js
+++ script/fadeHandler.js
@@ -1,25 +1,25 @@
 
-let project_container = document.getElementById('project_container');
+// let project_container = document.getElementById('project_container');
 
-const fadeOutOnScroll = (element) => {
-    if (!element) {
-        return;
-    }
-    var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-    var elementHeight = element.offsetHeight;
-    var scrollTop = document.documentElement.scrollTop;
-    var opacity = 1;
-    if (scrollTop > distanceToTop) {
-        opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
-    }
-    if (opacity >= 0) {
-        element.style.opacity = opacity;
-    }
-}
+// const fadeOutOnScroll = (element) => {
+//     if (!element) {
+//         return;
+//     }
+//     var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+//     var elementHeight = element.offsetHeight;
+//     var scrollTop = document.documentElement.scrollTop;
+//     var opacity = 1;
+//     if (scrollTop > distanceToTop) {
+//         opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+//     }
+//     if (opacity >= 0) {
+//         element.style.opacity = opacity;
+//     }
+// }
 
-const scrollHandler = () => {
-    fadeOutOnScroll(project_container);
-}
+// const scrollHandler = () => {
+//     fadeOutOnScroll(project_container);
+// }
 
-window.addEventListener('scroll', scrollHandler);
+// window.addEventListener('scroll', scrollHandler);
 

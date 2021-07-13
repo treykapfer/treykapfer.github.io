d43372aa2967545d3ce91ff589bffe3630d0e762
Trey K <80800860+treykapfer@users.noreply.github.com>
2021-07-13 14:13:53 -0500

    infoBoxHandler finished

--- script/infoBoxHandler.js
+++ script/infoBoxHandler.js
@@ -2,4 +2,16 @@ const infoBoxHandler = (clickedId) => {
     thisId = document.getElementById(clickedId).id;
     infoBox = document.getElementById('info_bar');
     console.log(thisId);
+    if (thisId === "phone_number") {
+        infoBox.innerHTML = "(913)669-0379";
+    }
+    else if (thisId === "email") {
+        infoBox.innerHTML = "treykapfer@gmail.com";
+    }
+    else if (thisId === "github") {
+        infoBox.innerHTML = "github.com/treykapfer";
+    }
+    else {
+        infoBox.innerHTML = "https://www.linkedin.com/in/trey-kapfer-252ab5103/";
+    }
 }

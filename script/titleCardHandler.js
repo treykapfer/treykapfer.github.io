0a52a99d4e2a3f4b3503f4c1a9f1034927db24ac
Trey K <80800860+treykapfer@users.noreply.github.com>
2021-07-13 16:19:29 -0500

    add title card handler and delete script

---
+++ script/titleCardHandler.js
@@ -0,0 +1,15 @@
+const titleCardArr = ["Front-End Engineer", "Full Stack Developer", "Designer", "Game Developer", "UX/UI Designer", "Hobbyist", "World Creator", "All Around Cool Guy"]
+let i = 0;
+
+titleCard = () => {
+    /*reset i if it gets too big*/
+    if (i >= titleCardArr.length) i = 0;
+    /*loop through the title card array*/
+    document.getElementById('scrolling_title').innerHTML = titleCardArr[i];
+    i++;
+    /*glitch animation*/
+
+    setTimeout(()=> {titleCard()}, 7000);
+}
+
+titleCard();

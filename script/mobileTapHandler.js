11290cf21e497a39f5e85baca2a60494bac16c80
Trey K <80800860+treykapfer@users.noreply.github.com>
2021-07-31 15:57:11 -0500

    fix javascript handler to include iphone

--- script/mobileTapHandler.js
+++ script/mobileTapHandler.js
@@ -2,7 +2,24 @@ const tapHandler = (clickedId) => {
     //check for mobile display
     let currentWidth = width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-    if (currentWidth <= 748) {
+    if (currentWidth <= 400) {
+        let currentId = document.getElementById(clickedId);
+        let currentPar = currentId.getElementsByTagName("p")[0];
+
+        if (currentId.style.height) {
+            currentId.style.removeProperty("height");
+            currentId.style.removeProperty("opacity");
+            currentPar.style.removeProperty("display");
+        }
+
+        else {
+            currentId.style.height = "296px"
+            currentId.style.opacity = "100"
+            currentPar.style.display = "none"
+        }
+    }
+    
+    else if (currentWidth <= 748) {
         let currentId = document.getElementById(clickedId);
         let currentPar = currentId.getElementsByTagName("p")[0];
 
@@ -18,4 +35,6 @@ const tapHandler = (clickedId) => {
             currentPar.style.display = "none"
         }
     }
+
+
 }
